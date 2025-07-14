fee6afbd2b8affcf72fc900dca245232107b43d4
supersaadd <mariasaad2003@gmail.com>
2025-07-14 12:54:26 +0300

    delete

--- crypto 3/ISIR/ZRR/collision.py
+++
@@ -1,8 +0,0 @@
-
-def read(i):
-    with open("/home/lucie/crypto/md5_collider/md5_collider/RES"+str(i),"rb") as f:
-        out = f.read()
-        print(out.hex())
-
-for i in range(1,5):
-    read(i)
